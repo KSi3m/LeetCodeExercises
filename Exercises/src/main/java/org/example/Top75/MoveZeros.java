dd3d74f0b3e6f99d5c38333f087921d354a952aa
ksi3m <mieszko9919@gmail.com>
2025-04-25 13:44:49 +0200

    Max number of K-Sum pairs (1679)

--- Exercises/src/main/java/org/example/Top75/MoveZeros.java
+++ Exercises/src/main/java/org/example/Top75/MoveZeros.java
@@ -5,46 +5,33 @@ import java.util.Map;
 
 public class MoveZeros {
 
-    public int maxOperations(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int count = 0;
+    public void moveZeroes(int[] nums) {
+        int leftPtr = 0;
+
         for(int i =0;i<nums.length;i++)
         {
-            int res = k - nums[i];
-            if(map.containsKey(res))
+            if(nums[i]!= 0)
             {
-                count++;
-                int amount = map.get(res);
-                if(amount == 1)
-                    map.remove(res);
-                else
-                    map.put(res,amount-1);
-            }
-            else{
-                map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+                int temp = nums[i];
+                nums[i] = nums[leftPtr];
+                nums[leftPtr] = temp;
+                leftPtr++;
             }
         }
-        return count;
+
     }
 }
 /*
-You are given an integer array nums and an integer k.
-In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
-Return the maximum number of operations you can perform on the array.
+Given an integer array nums, move all 0's to the end of it while
+maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
 
 Example 1:
 
-Input: nums = [1,2,3,4], k = 5
-Output: 2
-Explanation: Starting with nums = [1,2,3,4]:
-- Remove numbers 1 and 4, then nums = [2,3]
-- Remove numbers 2 and 3, then nums = []
-There are no more pairs that sum up to 5, hence a total of 2 operations.
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
 Example 2:
 
-Input: nums = [3,1,3,4,3], k = 6
-Output: 1
-Explanation: Starting with nums = [3,1,3,4,3]:
-- Remove the first two 3's, then nums = [1,4,3]
-There are no more pairs that sum up to 6, hence a total of 1 operation.
+Input: nums = [0]
+Output: [0]
  */
